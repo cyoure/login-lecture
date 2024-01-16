95427e81e5c526927fff0471c1d3e016507cacdc
cyoure <wwj9559@gmail.com>
2024-01-16 11:22:41 +0900

    프런트 단 javascript 파일 만들기

--- app/app.js
+++ app/app.js
@@ -9,6 +9,7 @@ const home = require("./src/routes/home"); // 라우팅
 // 웹 세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`));
 
 app.use("/", home); // use 미들웨어
 
