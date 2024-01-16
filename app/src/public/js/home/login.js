45c7921eabc6ef9e7c24dd0827db52e46a9373b0
cyoure <wwj9559@gmail.com>
2024-01-16 15:37:37 +0900

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,15 +1,21 @@
 "use strict";
 
 const id = document.querySelector("#id"),
-    psword = document.querySelector("#psword"),
-    loginBin = document.querySelector("button");
+  psword = document.querySelector("#psword"),
+  loginBin = document.querySelector("button");
 
 loginBin.addEventListener("click", login);
 
-function login(){
-    const req = {
-        id : id.value
-    };
-    console.log(req);
+function login() {
+  const req = {
+    id: id.value,
+    psword: psword.value,
+  };
+  fetch("/login", {
+    method: "POST",
+    headers : {
+        "Content-Type": "application/json",
+    },
+    body: JSON.stringify(req),
+  });
 }
-
