409d7c59f85dcb4c9dfbe12aa8ced0a93e458949
cyoure <wwj9559@gmail.com>
2024-01-16 17:14:53 +0900

    백엔드에서 로그인 기능 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,20 +1,38 @@
 "use strict";
 
+const users = {
+  id: ["wwj9559", "카리나", "원터"],
+  psword: ["1234", "1234", "123456"],
+};
+
 const output = {
   hello: (req, res) => {
     res.render("home/index");
   },
-
   login: (req, res) => {
     res.render("home/login");
   },
 };
-
 const process = {
-  login: (req,res) => {
-    console.log(req.body);
+  login: (req, res) => {
+    console.log(req.body)
+    const id = req.body.id,
+      psword = req.body.psword;
+
+    if (users.id.includes(id)) {
+      const idx = users.id.indexOf(id);
+      if (users.psword[idx] === psword) {
+        return res.json({
+          success: true,
+        });
+      }
+    }
+    return res.json({
+      success : false,
+      msg : "로그인에 실패하셨습니다.",
+    });
   }
-}
+};
 
 module.exports = {
   output,
